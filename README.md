1f8e21ccac5df652274d1681b8fae340148e28cc
bhafer <bhafer@github.com>
2014-07-29 23:23:36 +0200

    Combined check-in of code, readme, and license for initial release.

--- README.md
+++ README.md
@@ -2,3 +2,51 @@ TitaniumBackupDecrypt
 =====================
 
 Decrypt Titanium Backup for Android encrypted .tar.gz files using PHP.
+
+Dependencies: <br/>
+&nbsp;&nbsp;&nbsp;&nbsp; http://phpseclib.sourceforge.net/ :: Install via PEAR with: <br/>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; pear channel-discover phpseclib.sourceforge.net <br/>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; pear install phpseclib/Crypt_AES phpseclib/Crypt_RSA
+
+Usage: <br/>
+&nbsp;&nbsp;&nbsp;&nbsp; php TitaniumBackupDecrypt <.tar.gz file>
+
+Based on file format specification information from Titanium Backup at: <br/>
+&nbsp;&nbsp;&nbsp;&nbsp; https://plus.google.com/+ChristianEgger/posts/MQBmYhKDex5
+
+=====================
+
+https://plus.google.com/+ChristianEgger/posts/MQBmYhKDex5 ::
+
+So, the file format for encrypted data backups is as follows:
+
+"TB_ARMOR_V1" '\n' passphraseHmacKey '\n' passphraseHmacResult '\n' publicKey '\n' encryptedPrivateKey '\n' encryptedSessionKey '\n' Data <br/>
+
+Each of the 5 "variables" (passphraseHmacKey, passphraseHmacResult, publicKey, encryptedPrivateKey, encryptedSessionKey) is stored in Base64 format without linewraps (of course) and can be decoded with: Base64.decode(passphraseHmacKey, Base64.NO_WRAP)
+
+Then the user-supplied passphrase (String) can be verified as follows: <br/>
+Mac mac = Mac.getInstance("HmacSHA1"); <br/>
+mac.init(new SecretKeySpec(passphraseHmacKey, "HmacSHA1")); <br/>
+byte[] sigBytes = mac.doFinal(passphrase.getBytes("UTF-8")); <br/>
+boolean passphraseMatches = Arrays.equals(sigBytes, passphraseHmacResult);
+
+Then the passphrase is independently hashed with SHA-1. We append [twelve] 0x00 bytes to the 160-bit result to constitute the 256-bit AES key which is used to decrypt "encryptedPrivateKey" (with an IV of [sixteen] 0x00 bytes). [Decrypt using AES-256 in CBC mode and perform PKCS5 unpadding. Note that AES-256 is equivalent to Rijndael-128 using a 256 bit key.]
+
+Then we build the KeyPair object as follows: <br/>
+KeyFactory keyFactory = KeyFactory.getInstance("RSA"); <br/>
+PrivateKey privateKey2 = keyFactory.generatePrivate(new PKCS8EncodedKeySpec(privateKey)); <br/>
+PublicKey publicKey2 = keyFactory.generatePublic(new X509EncodedKeySpec(publicKey)); <br/>
+KeyPair keyPair = new KeyPair(publicKey2, privateKey2);
+
+Then we decrypt the session key as follows: <br/>
+Cipher rsaDecrypt = Cipher.getInstance("RSA/NONE/PKCS1Padding");
+rsaDecrypt.init(Cipher.DECRYPT_MODE, keyPair.getPrivate()); <br/>
+ByteArrayOutputStream baos = new ByteArrayOutputStream(); <br/>
+CipherOutputStream cos = new CipherOutputStream(baos, rsaDecrypt); <br/>
+cos.write(encryptedSessionKey); <br/>
+cos.close(); <br/>
+byte[] sessionKey = baos.toByteArray();
+
+And finally, we decrypt the data itself with the session key (which can be either a 128-bit, 192-bit or 256-bit key) and with a 0x00 IV.
+
+While the "zero" IV is suboptimal from a security standpoint, it allows files to be encoded faster - because every little bit counts, especially when we store backups with LZO compression.
---
+++ TitaniumBackupDecrypt.php
@@ -0,0 +1,169 @@
+<?php
+// ---------------------------
+//
+// Decrypt Titanium Backup for Android encrypted .tar.gz files using PHP.
+//
+// Dependencies:
+//     http://phpseclib.sourceforge.net/ :: Install via PEAR with:
+//         pear channel-discover phpseclib.sourceforge.net
+//         pear install phpseclib/Crypt_AES phpseclib/Crypt_RSA
+//
+// Usage:
+//     php TitaniumBackupDecrypt <.tar.gz file>
+//
+// Based on file format specification information from Titanium Backup at:
+//     https://plus.google.com/+ChristianEgger/posts/MQBmYhKDex5
+//
+// ---------------------------
+//
+// The MIT License (MIT)
+//
+// Copyright (c) 2014 Brian T. Hafer
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+// ---------------------------
+
+require_once('Crypt/AES.php');
+require_once('Crypt/RSA.php');
+
+define('TB_BUFFER_SIZE', 128 * 2048);
+
+function pkcs5_unpad($text) {
+    $pad = ord($text{strlen($text)-1});
+    if ($pad > strlen($text)) return false;
+    if (strspn($text, chr($pad), strlen($text) - $pad) != $pad) return false;
+    return substr($text, 0, -1 * $pad);
+}
+
+$filenameIn = $argv[1];
+$fileIn = fopen($filenameIn, 'rb');
+if ($fileIn === false) {
+    echo "File not found: $filenameIn";
+    exit();
+}
+
+$header = fgets($fileIn, 64);
+$header = rtrim($header, "\n");
+echo "File type: $header \n";
+if ($header != "TB_ARMOR_V1") {
+    echo "Unsupported file type. Expected: \"TB_ARMOR_V1\".";
+    exit();
+}
+
+$passphraseHmacKey = fgets($fileIn, 1024);
+$passphraseHmacKey = rtrim($passphraseHmacKey, "\n");
+//echo "Passphrase HMAC Key: $passphraseHmacKey \n";
+$passphraseHmacKey = base64_decode($passphraseHmacKey, true);
+if ($passphraseHmacKey === false) {
+    echo "Invalid Passphrase HMAC Key.";
+    exit();
+}
+
+$passphraseHmacResult = fgets($fileIn, 1024);
+$passphraseHmacResult = rtrim($passphraseHmacResult, "\n");
+//echo "Passphrase HMAC Result: $passphraseHmacResult \n";
+$passphraseHmacResult = base64_decode($passphraseHmacResult, true);
+if ($passphraseHmacResult === false) {
+    echo "Invalid Passphrase HMAC Result.";
+    exit();
+}
+
+$publicKey = fgets($fileIn, 1024);
+$publicKey = rtrim($publicKey, "\n");
+//echo "Public Key: $publicKey \n";
+$publicKey = base64_decode($publicKey, true);
+if ($publicKey === false) {
+    echo "Invalid Public Key.";
+    exit();
+}
+
+$encryptedPrivateKey = fgets($fileIn, 4096);
+$encryptedPrivateKey = rtrim($encryptedPrivateKey, "\n");
+//echo "Encrypted Private Key: $encryptedPrivateKey \n";
+$encryptedPrivateKey = base64_decode($encryptedPrivateKey, true);
+if ($encryptedPrivateKey === false) {
+    echo "Invalid Encrypted Private Key.";
+    exit();
+}
+
+$encryptedSessionKey = fgets($fileIn, 1024);
+$encryptedSessionKey = rtrim($encryptedSessionKey, "\n");
+//echo "Encrypted Session Key: $encryptedSessionKey \n";
+$encryptedSessionKey = base64_decode($encryptedSessionKey, true);
+if ($encryptedSessionKey === false) {
+    echo "Invalid Encrypted Session Key.";
+    exit();
+}
+
+// @TODO Would be much better if password were hidden on the command line.
+$prompt = "Enter encryption passphrase: ";
+if (PHP_OS == 'WINNT') {
+  echo $prompt;
+  $passphrase = stream_get_line(STDIN, 1024, PHP_EOL);
+} else {
+  $passphrase = readline($prompt);
+}
+
+if ($passphraseHmacResult != hash_hmac('sha1', $passphrase, $passphraseHmacKey, true)) {
+    echo "Supplied passphrase not valid for encrypted file.";
+    exit();
+}
+
+$aesKey = sha1($passphrase, true) . "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00";
+if (strlen($aesKey) * 8 != 256) {
+    echo "Error generating AES key from supplied passphrase.";
+    exit();
+}
+
+$aesCipher = new Crypt_AES(CRYPT_AES_MODE_CBC);
+$aesCipher->setKey($aesKey);
+$aesCipher->setIV("\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00");
+$decryptedPrivateKey = $aesCipher->decrypt($encryptedPrivateKey);
+// Other ways to do this:
+// $decryptedPrivateKey = openssl_decrypt($encryptedPrivateKey, 'aes-256-cbc', $aesKey, true, "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00");
+// $decryptedPrivateKey = pkcs5_unpad(mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $aesKey, $encryptedPrivateKey, MCRYPT_MODE_CBC, "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"));
+//echo "Decrypted Private Key: " . base64_encode($decryptedPrivateKey) . " \n";
+
+$rsaCipher = new Crypt_RSA();
+$rsaCipher->setEncryptionMode(CRYPT_RSA_ENCRYPTION_PKCS1);
+$rsaCipher->loadKey($decryptedPrivateKey);
+$decryptedSessionKey = $rsaCipher->decrypt($encryptedSessionKey);
+//echo "Decrypted Session Key: " . base64_encode($decryptedSessionKey) . " \n";
+echo "Session Key Length: " . strlen($decryptedSessionKey) * 8 . " \n";
+
+// Create output file.
+$filenameOut = dirname($argv[1]) . DIRECTORY_SEPARATOR . basename($argv[1], '.tar.gz') . '-decrypted.tar.gz';
+$fileOut = fopen($filenameOut, 'wb');
+echo "Writing file: $filenameOut\n";
+
+// Setup cipher for continuous buffering and copy between files.
+$aesCipher->setKey($decryptedSessionKey);
+$aesCipher->enableContinuousBuffer();
+$aesCipher->disablePadding();
+while (!feof($fileIn)) {
+    fwrite($fileOut, $aesCipher->decrypt(fread($fileIn, TB_BUFFER_SIZE)));
+}
+
+fclose($fileIn);
+fclose($fileOut);
+
+echo "Done.";
+
+?>
